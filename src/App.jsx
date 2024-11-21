1d55e424ab8279853682b7889fc8022191d0d21f
Anderson Zambrano <Andersonzm17@gmail.com>
2024-11-21 19:23:03 -0300

    Subiendo el login y el register

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Header from './components/header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Row, Col } from 'react-bootstrap';
 import CardPizza from './components/CardPizza';
-
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Header></Header>
-      <Row>
+      <Register></Register>
+      <Login></Login>
+      {/*<Row>
         <Col>
           <CardPizza nombre="4 quesos" descripcion="Esta pizza tiene 4 quesos" img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"
           precio={5950}></CardPizza>
@@ -24,7 +27,8 @@ function App() {
           <CardPizza nombre="Peperoni" descripcion="Esta pizza tiene peperoni" img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_com.jpg?alt=media&token=e7cde87a-08d5-4040-ac54-90f6c31eb3e3"
           precio={6950}></CardPizza>
         </Col>
-      </Row>
+      </Row>*/}
+
      
       <Footer></Footer>
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer style={footerStyle}>
+      <div style={containerStyle}>
+        <p style={textStyle}>
+          © 2021 - Pizzeria Mamma Mia! - Todos los derechos reservados.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+// Estilos CSS inline
+const footerStyle = {
+  backgroundColor: "#282c34",
+  color: "#ffffff",
+  padding: "20px 0",
+  textAlign: "center",
+};
+
+const containerStyle = {
+  maxWidth: "1000px",
+  margin: "0 auto",
+  padding: "0 20px",
+};
+
+const textStyle = {
+  margin: "0 0 10px 0",
+  fontSize: "15px",
+};
+
+
+
+
+
+
+export default Footer;
